85ac94e67b50a1953969160e2104b7f63a4672c1
BEKA2222 <hiro21874@gmail.com>
2025-12-17 15:15:22 +0600

    update

--- glovo/urls.py
+++ glovo/urls.py
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('glovo_app.urls')),
+    path('', include('glovo_app.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
